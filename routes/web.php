c83f0b036a67b66a507254850cfc439d9d55642c
Davide Francavilla <davfrancavilla@gmail.com>
2020-11-11 10:29:15 +0100

    push

--- routes/web.php
+++ routes/web.php
@@ -33,10 +33,8 @@ Route::get('/', function () {
     return view('index')->name('homepage');
 });
 
+Route::get('/home', 'HomeController@index')->name('home');
+
 // Route::get('logout', 'Auth\LoginController@logout');
 
 
-
-
-
-Route::get('/home', 'HomeController@index')->name('home');
