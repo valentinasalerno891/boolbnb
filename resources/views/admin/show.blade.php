dacb85113cdc11bbc41bc2711f77a6f7144b7369
mattia <mattia24.studio@gmail.com>
2020-11-18 22:14:17 +0100

    apartment.show

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -14,74 +14,75 @@
                 <button type="submit"><span>Delete <i class="far fa-trash-alt"></i></span></button>
             </form>
           @endif
-        </div>   
+        </div>  
       </div> 
       <div class="image_apartment">
         <img src="https://media.istockphoto.com/photos/evening-view-of-a-modern-house-with-swimming-pool-picture-id1151833014" alt="apartment">  
       </div> 
       <hr>
-        <div class="description_information">
-          <div class="apartment_description">
-            <h3>DESCRIZIONE APPARTAMENTO</h3>
-            <span>{{$apartment->description}}</span>
-          </div>
-          <div class="apartment_information">
-            <h3>INFORMAZIONI APPARTAMENTO</h3>
-            <p class="">Metri quadri appartamento : {{$apartment->square_meters}}</p>
-            <p class="">Camere : {{$apartment->rooms}} <i class="fas fa-door-open"></i></p>
-            <p class="">Letti : {{$apartment->beds}} <i class="fas fa-bed"></i></p>
-            <p class="">Bagni : {{$apartment->bathrooms}} <i class="fas fa-bath"></i></p>
-            <p class="">Proprietario : {{$apartment->user->name}} {{$apartment->user->lastname}} <i class="fas fa-user"></i></p>
-          </div>
+      <div class="description_information">
+        <div class="apartment_description">
+          <h3>DESCRIZIONE APPARTAMENTO</h3>
+          <span>{{$apartment->description}}</span>
         </div>
         <hr>
-        <div class="service_maps"> 
-          <div class="apartment_service">
-            <h3>SERVIZI APPARTAMENTO</h3>
-            <ul>
-              @foreach ($apartment->services  as $service)
-                @switch($service->id)
-                @case($service->id==1)
-                <li><i class="fas fa-wifi"></i>{{$service->name}}</li>
-                @break
-                @case($service->id==2)
-                <li><i class="fas fa-car"></i>{{$service->name}}</li>
-                @break
-                @case($service->id==3)
-                  <li><i class="fas fa-swimmer"></i> {{$service->name}}</li>
-                @break
-                @case($service->id==4)
-                <li><i class="fas fa-door-open"></i> {{$service->name}}</li>
-                @break
-                @case($service->id==5)
-                <li><i class="fas fa-hot-tub"></i> {{$service->name}}</li>
-                @break
-                @case($service->id==6)
-                <li><i class="fas fa-water"></i> {{$service->name}}</li>
-                @break
-                @default
-                  <li>NESSUN SERVIZIO PRESENTE</li>
-                @endswitch
-
-              @endforeach
-            </ul>
-          </div>
-          <div class="apartment_maps">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d179094.73272925883!2d9.037648910709462!3d45.46264823810021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786c1493f1275e7%3A0x3cffcd13c6740e8d!2sMilano%20MI!5e0!3m2!1sit!2sit!4v1605616090069!5m2!1sit!2sit" width="500" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-          </div>
+        <div class="apartment_information">
+          <h3>INFORMAZIONI APPARTAMENTO</h3>
+          <p class="">Metri quadri appartamento : {{$apartment->square_meters}}</p>
+          <p class="">Camere : {{$apartment->rooms}} <i class="fas fa-door-open"></i></p>
+          <p class="">Letti : {{$apartment->beds}} <i class="fas fa-bed"></i></p>
+          <p class="">Bagni : {{$apartment->bathrooms}} <i class="fas fa-bath"></i></p>
+          <p class="">Proprietario : {{$apartment->user->name}} {{$apartment->user->lastname}} <i class="fas fa-user"></i></p>
         </div>
-        <hr>         
+      </div>
+      <hr>
+      <div class="service_maps"> 
+        <div class="apartment_service">
+          <h3>SERVIZI APPARTAMENTO</h3>
+          <ul>
+            @foreach ($apartment->services  as $service)
+              @switch($service->id)
+              @case($service->id==1)
+              <li><i class="fas fa-wifi"></i>{{$service->name}}</li>
+              @break
+              @case($service->id==2)
+              <li><i class="fas fa-car"></i>{{$service->name}}</li>
+              @break
+              @case($service->id==3)
+                <li><i class="fas fa-swimmer"></i> {{$service->name}}</li>
+              @break
+              @case($service->id==4)
+              <li><i class="fas fa-door-open"></i> {{$service->name}}</li>
+              @break
+              @case($service->id==5)
+              <li><i class="fas fa-hot-tub"></i> {{$service->name}}</li>
+              @break
+              @case($service->id==6)
+              <li><i class="fas fa-water"></i> {{$service->name}}</li>
+              @break
+              @default
+                <li>NESSUN SERVIZIO PRESENTE</li>
+              @endswitch
+            @endforeach
+          </ul>
+        </div>
+        <hr>
+        <div class="apartment_maps">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d89547.26437699102!2d9.10769243625494!3d45.46271244817566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786c1493f1275e7%3A0x3cffcd13c6740e8d!2sMilano%20MI!5e0!3m2!1sit!2sit!4v1605696355367!5m2!1sit!2sit"  height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+      </div>
+      <hr>         
       @if ($apartment->user_id != Auth::id()) 
       @if (session('status'))
       <div class="alert alert-success">
           {{ session('status') }}
       </div>
       @endif
-      <form action="{{route('messages.store',$apartment->id)}}" method="post">
+      <form class="message" action="{{route('messages.store',$apartment->id)}}" method="post">
         @csrf
         @method('POST')
       <div class="form-group">
-        label for="email">Email</label>
+        <label for="email">Email</label>
         @auth
           <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Inserisci la tua email" value="{{Auth::user()->email}}">
         @endauth
@@ -97,11 +98,10 @@
         <label for="body">Messaggio</label>
         <input name="body" type="text" class="form-control" id="body" placeholder="Inserisci il messaggio">
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">Submit <i class="far fa-paper-plane"></i></button>
       </form>
+      <hr>
       @endif
     </div>
   </div>  
 @endsection
-
-  
