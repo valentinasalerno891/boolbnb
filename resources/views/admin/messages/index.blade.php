c77e06c0c3ee854f20e9c91b12461ab2949ed462
AndreaFedeli <andrea.fedeli159@gmail.com>
2020-11-18 16:40:09 +0100

    messages

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -1,9 +1,30 @@
+@extends('layouts.app')
+
+@section('title', 'Login')
+@section('content')
+  <div class="container messages-index">
+    <h2>Hai ricevuto questi messaggi per i tuoi appartamenti</h2>
+
 @foreach ($apartments as $apartment)
-    <h1>{{$apartment->title}}</h1>
+  <table class="table table-bordered">
+  <tbody>
+    <h3>{{$apartment->title}}</h3>
     @foreach ($apartment->messages as $message)
-        <ul>
+
+     <tr>
+      <td>{{$message->title}}</td>
+      <td>{{$message->email}}</td>
+      <td>{{$message->body}}</td>
+     </tr>
+        {{-- <ul>
             <li><a href="{{ route('message.show',$message->id) }}">{{$message->title}}</a></li>
             <li>{{$message->email}}</li>
-        </ul>
+        </ul> --}}
+
+      </div>
     @endforeach
+  </tbody>
+  </table>
 @endforeach
+
+@endsection
