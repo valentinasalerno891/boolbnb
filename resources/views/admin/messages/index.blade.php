08960c11e9f25136553451fcaa9fc152b3c055ce
valentina891 <valemil.vs@gmail.com>
2020-11-21 17:34:21 +0100

    ancora frontend

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -2,37 +2,29 @@
 
 @section('title', 'Login')
 @section('content')
-  <div class="container messages-index">
-    <h2>I tuoi appartamenti hanno ricevuto questi messaggi</h2>
+    <div class="container messages-index col-md-8">
+        <h2>I tuoi messaggi</h2>
 
 @foreach ($apartments as $apartment)
-  <table class="table table-bordered">
-
-    <h3>{{$apartment->title}}</h3>
-    <tbody>
-      <thead>
-        <tr>
-          <th scope="col">Titolo</th>
-          <th scope="col">Mittente</th>
-          <th scope="col">Messaggio</th>
-
-        </tr>
-
-
-    @foreach ($apartment->messages as $message)
-
-     <tr>
-      <td>{{$message->title}}</td>
-      <td>{{$message->email}}</td>
-      <td>{{$message->body}}</td>
-     </tr>
-
-
-      </div>
-    @endforeach
-    </thead>
-  </tbody>
-  </table>
+        <table class="table table-bordered">
+            <h3>{{$apartment->title}}</h3>
+            <tbody>
+                <thead>
+                    <tr>
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Mittente</th>
+                        <th scope="col">Messaggio</th>
+                    </tr>
+                    @foreach ($apartment->messages as $message)
+                        <tr>
+                            <td>{{$message->title}}</td>
+                            <td>{{$message->email}}</td>
+                            <td>{{$message->body}}</td>
+                        </tr>
+                    @endforeach
+                </thead>
+            </tbody>
+        </table>
 @endforeach
-
+    </div>
 @endsection
--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -2,7 +2,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
                 <div class="logo">
-                    <img class="navbar-brand" src="https://cdn.freelogovectors.net/wp-content/uploads/2016/12/airbnb_logo.png" onclick="window.location='/home'"}}" alt="logo">
+                    <img class="navbar-brand pointer" src="https://cdn.freelogovectors.net/wp-content/uploads/2016/12/airbnb_logo.png" onclick="window.location='/home'"}} alt="logo">
                 </div>
                 {{-- <span class="navbar-brand">BoolBnB</span> --}}
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,7 +11,7 @@
                 <div class="home-menu collapse navbar-collapse" id="navbarSupportedContent">
                     @guest
                         <a class="home-menu-a nav-link nav-item m-3" href="{{ route('register') }}">Registrati</a>
-                        <a class="home-menu-a m-3" href="{{ route('login') }}">Login</a>
+                        <a class="home-menu-a m-3" href="{{ route('login') }}">Accedi</a>
                     @endguest
                         <a class="home-menu-a m-3" href="{{ route('apartments.create') }}">Inserisci un appartamento</a>
                     @auth
